8095b89a4e8f9dc4db6768f3424e058d95c83003
Eric Klinkhammer <eklinkh@amazon.com>
2019-10-13 16:04:32 -0700

    Commenting out all use of the annotations to see what exactly is building locally vs cloud

--- lambda/src/user.ts
+++ lambda/src/user.ts
@@ -1,23 +1,29 @@
 //import dynamo = require('aws-sdk/client-dynamodb-v2-node');
 
-import {PracticeSession} from './PracticeSession';
+//import {PracticeSession} from './PracticeSession';
 import {DataMapper} from '@aws/dynamodb-data-mapper';
 import DynamoDB = require('aws-sdk/clients/dynamodb');
 
 const client = new DynamoDB({region: 'us-west-2'});
 const mapper = new DataMapper({client});
 
+/**
+ * API Endpoint GET /users/{user_id}
+ * 
+ * Returns the 10 most recent practice sessions for the specified user, or
+ * 400 if the user does not exist.
+ */
 export const getUser = async (event: any={}) : Promise <any> => {
     console.log("Logging from getUser");
     console.log(event);
 
     console.log(event.queryStringParameters.user_id);
 
-    let session = new PracticeSession();
-    session.user = event.queryStringParameters.user_id;
+    //let session = new PracticeSession();
+    //session.user = event.queryStringParameters.user_id;
 
-    const fetched = await mapper.get({item: session});
-    console.log(fetched);
+    //const fetched = await mapper.get({item: session});
+    //console.log(fetched);
 				  
     return {
 	statusCode: 200,
@@ -29,14 +35,14 @@ export const postUser = async (event: any={}) : Promise <any> => {
     console.log("Logging from postUser");
     console.log(event);
 
-    let session = new PracticeSession();
-    session.user = event.body.userId
+    //let session = new PracticeSession();
+    //session.user = event.body.userId
 
-    console.log(session);
+    //console.log(session);
     
-    mapper.put({item: session}).then(() => {
-	console.log(session.user);
-    });
+    //mapper.put({item: session}).then(() => {
+//	console.log(session.user);
+   // });
     
     return {
 	statusCode: 200,
